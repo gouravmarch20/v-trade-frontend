f53d928ba0a5ad00656489157e04061d2c6f2ce9
gourav <gaurav.mishra@kogo.ai>
2024-09-19 19:59:01 +0530

    verified token

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -15,20 +15,20 @@ export const sendEmail = async({email, emailType, userId}:any) => {
             await User.findByIdAndUpdate(userId, 
                 {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
         }
-
+        console.log("the_login" , email)
         var transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
-              user: "3fd364695517df",
-              pass: "7383d58fd399cf"
+              user: "6fe575f7d4b269",
+              pass: "dbaccc28d09dc4"
               //TODO: add these credentials to .env file
             }
           });
 
 
         const mailOptions = {
-            from: 'hitesh@gmail.com',
+            from: 'gouravmarch20@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
